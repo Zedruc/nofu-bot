08bfd790dc20de91a2d96eed9955c2f155ec13f0
Zedruc <69540929+Zedruc@users.noreply.github.com>
2020-10-01 14:55:50 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js');
+const { member, mentions } = message
 
 module.exports = {
     name: 'kick',
     description: 'Will kick the mentioned member // Wird den gepingten Member kicken',
-    execute(message, args, member, mentions){
-        if (member.hasPermission('ADMINISTRATOR') || member.hasPermission('BAN_MEMBERS')) {
+    execute(message, args){
+        if (message.author.hasPermission('ADMINISTRATOR') || message.author.hasPermission('BAN_MEMBERS')) {
             console.log("yes it works");
         }else {
             message.channel.send(`<@${member.id}> You don't have permissions to use %ban!`)
