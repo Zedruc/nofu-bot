fd703a6012c9469290c418f3f970c8a1634e2cb1
Zedruc <69540929+Zedruc@users.noreply.github.com>
2020-10-03 17:56:00 +0200

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -5,24 +5,19 @@ require("moment-duration-format");
 
 require("moment-duration-format");
 module.exports = {
-    name: 'uptime',
-    description: "Bot Stats",
-    execute(message, args, bot){
+  name: 'uptime',
+  description: "Bot Stats",
+  execute(message, args, bot) {
 
+    let days = Math.floor(client.uptime / 86400000);
+    let hours = Math.floor(client.uptime / 3600000) % 24;
+    let minutes = Math.floor(client.uptime / 60000) % 60;
+    let seconds = Math.floor(client.uptime / 1000) % 60;
 
-          const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-
-        //#region Testing different options of uptime
-          const x = Date.now();
-          const xy = x - Date.now();
-          console.log(xy);
-        //#endregion
-
-          const statEmbed = new Discord.MessageEmbed()
-          .setTitle("**  = STATISTICS =**")
-          .addField("**Memory usage ::**", `**${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB**`)
-          .addField("**Uptime (glitched, still trying to fix it ;-;)**", `**${duration}**`);
-
-          message.channel.send(statEmbed)
-        }
+    const statEmbed = new Discord.MessageEmbed()
+      .setTitle("**  = STATISTICS =**")
+      .addField("**Memory usage ::**", `**${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB**`)
+      .addField("**Uptime (glitched, still trying to fix it ;-;)**", `__Uptime:__\n${days}d ${hours}h ${minutes}m ${seconds}s`);
+    message.channel.send(statEmbed);
+  }
 }
