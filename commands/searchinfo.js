97bf8bc05cf672469c5c5b1171ff2549cdeba927
Zedruc <69540929+Zedruc@users.noreply.github.com>
2020-10-04 15:01:24 +0200

    Update searchinfo.js

--- commands/searchinfo.js
+++ commands/searchinfo.js
@@ -5,11 +5,12 @@ module.exports = {
     name: 'searchinfo',
     description: 'shows all genre the user can look up yet',
     execute(message, args) {
-        let genreInfo = new Discord.MessageEmbed()
+        let genreInfoEmbed = new Discord.MessageEmbed()
             .setTitle("__**Genre Search Center**__")
             .setThumbnail(image_links.anime_genre_center)
             .setDescription('Here are all Genres you can search: \n many more will be added :D \n')
             .addField(" Action  • Adventure  • Demons \n  Drama  • Horror  • Mecha \n  Romance  • School  • Shounen", "These are all genres you can search yet")
 
+        message.channel.send(genreInfoEmbed);
     }
 }
