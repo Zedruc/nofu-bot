1800200ac2489b0ecd6af2f4782e3baa52b028f8
Zedruc <69540929+Zedruc@users.noreply.github.com>
2020-10-04 14:37:47 +0200

    HELP

--- commands/findAnimeInfo.js
+++ commands/findAnimeInfo.js
@@ -8,39 +8,305 @@ module.exports = {
 
         let prefix = "%";
         let msgArgs = message.content.slice(prefix.length).trim().split(/ +/);
+
+        const searchScripts = fs.readdirSync('../commands/animeSearch').filter(file => file.endsWith('.js'));
+        for (const file of searchScripts) {
+            const command = require(`./commands/animeSearch/${file}`);
+
+            client.commands.set(command.name, command);
+        }
+
+
         // let searchCriteria = msgArgs[1];
         let rawArgs = args;
         let argsString = rawArgs.toString();
         let animeName = argsString.replace(/,/g, " ");
         console.log(argsString);
 
-        //if (searchCriteria == "name") {
-        https.get('https://api.jikan.moe/v3/search/anime?q=' + animeName, res => {
+        if (args.length > 1) {
+            https.get('https://api.jikan.moe/v3/search/anime?q=' + animeName, res => {
 
-            let body = '';
+                let body = '';
 
-            res.on('data', chunk => {
-                body += chunk;
-            });
+                res.on('data', chunk => {
+                    body += chunk;
+                });
 
-            res.on('end', () => {
-                let bodyString = JSON.parse(body);
-                console.log(bodyString.results[0].title);
-                let topResult = bodyString.results[0];
+                res.on('end', () => {
+                    let bodyString = JSON.parse(body);
+                    console.log(bodyString.results[0].title);
+                    let topResult = bodyString.results[0];
 
-                let animeInfoEmbed = new Discord.MessageEmbed()
-                    .setTitle("**Top search results for __" + animeName + "__**")
-                    .setThumbnail(topResult.image_url)
-                    .setDescription(topResult.synopsis)
-                    .addField("Episodes:", topResult.episodes, true)
-                    .addField("Release Date:", topResult.start_date)
+                    let animeInfoEmbed = new Discord.MessageEmbed()
+                        .setTitle("**Top search results for __" + animeName + "__**")
+                        .setThumbnail(topResult.image_url)
+                        .setDescription(topResult.synopsis)
+                        .addField("Episodes:", topResult.episodes, true)
+                        .addField("Release Date:", topResult.start_date)
 
 
-                message.channel.send(animeInfoEmbed);
-                console.log(msgArgs);
-            });
-        })
-        //} else if (searchCriteria == "genre")
+                    message.channel.send(animeInfoEmbed);
+                    console.log(msgArgs);
+                });
+            })
+        } else {
+            //huge genre check...
+            switch (args[1]) {
+                case 'action':
+                    https.get('https://api.jikan.moe/v3/search/anime?genre=1', res => {
+
+                        let body = '';
+
+                        res.on('data', chunk => {
+                            body += chunk;
+                        });
+
+                        res.on('end', () => {
+                            let bodyString = JSON.parse(body);
+                            console.log(bodyString.results[0].title);
+                            let topResult = bodyString.results[0];
+
+                            let animeInfoEmbed = new Discord.MessageEmbed()
+                                .setTitle("**Top search results for __Action__**")
+                                .setThumbnail(topResult.image_url)
+                                .setDescription(topResult.synopsis)
+                                .addField("Episodes:", topResult.episodes, true)
+                                .addField("Release Date:", topResult.start_date)
+
+
+                            message.channel.send(animeInfoEmbed);
+                            console.log(msgArgs);
+                        });
+                    })
+                    break;
+
+                case 'adventure':
+                    https.get('https://api.jikan.moe/v3/search/anime?genre=2', res => {
+
+                        let body = '';
+
+                        res.on('data', chunk => {
+                            body += chunk;
+                        });
+
+                        res.on('end', () => {
+                            let bodyString = JSON.parse(body);
+                            console.log(bodyString.results[0].title);
+                            let topResult = bodyString.results[0];
+
+                            let animeInfoEmbed = new Discord.MessageEmbed()
+                                .setTitle("**Top search results for __Action__**")
+                                .setThumbnail(topResult.image_url)
+                                .setDescription(topResult.synopsis)
+                                .addField("Episodes:", topResult.episodes, true)
+                                .addField("Release Date:", topResult.start_date)
+
+
+                            message.channel.send(animeInfoEmbed);
+                            console.log(msgArgs);
+                        });
+                    })
+                    break;
+
+                case 'demons':
+                    https.get('https://api.jikan.moe/v3/search/anime?genre=6', res => {
+
+                        let body = '';
+
+                        res.on('data', chunk => {
+                            body += chunk;
+                        });
+
+                        res.on('end', () => {
+                            let bodyString = JSON.parse(body);
+                            console.log(bodyString.results[0].title);
+                            let topResult = bodyString.results[0];
+
+                            let animeInfoEmbed = new Discord.MessageEmbed()
+                                .setTitle("**Top search results for __Action__**")
+                                .setThumbnail(topResult.image_url)
+                                .setDescription(topResult.synopsis)
+                                .addField("Episodes:", topResult.episodes, true)
+                                .addField("Release Date:", topResult.start_date)
+
+
+                            message.channel.send(animeInfoEmbed);
+                            console.log(msgArgs);
+                        });
+                    })
+                    break;
+
+                case 'drama':
+                    https.get('https://api.jikan.moe/v3/search/anime?genre=8', res => {
+
+                        let body = '';
+
+                        res.on('data', chunk => {
+                            body += chunk;
+                        });
+
+                        res.on('end', () => {
+                            let bodyString = JSON.parse(body);
+                            console.log(bodyString.results[0].title);
+                            let topResult = bodyString.results[0];
+
+                            let animeInfoEmbed = new Discord.MessageEmbed()
+                                .setTitle("**Top search results for __Action__**")
+                                .setThumbnail(topResult.image_url)
+                                .setDescription(topResult.synopsis)
+                                .addField("Episodes:", topResult.episodes, true)
+                                .addField("Release Date:", topResult.start_date)
+
+
+                            message.channel.send(animeInfoEmbed);
+                            console.log(msgArgs);
+                        });
+                    })
+                    break;
+
+                case 'horror':
+                    https.get('https://api.jikan.moe/v3/search/anime?genre=14', res => {
+
+                        let body = '';
+
+                        res.on('data', chunk => {
+                            body += chunk;
+                        });
+
+                        res.on('end', () => {
+                            let bodyString = JSON.parse(body);
+                            console.log(bodyString.results[0].title);
+                            let topResult = bodyString.results[0];
+
+                            let animeInfoEmbed = new Discord.MessageEmbed()
+                                .setTitle("**Top search results for __Action__**")
+                                .setThumbnail(topResult.image_url)
+                                .setDescription(topResult.synopsis)
+                                .addField("Episodes:", topResult.episodes, true)
+                                .addField("Release Date:", topResult.start_date)
+
+
+                            message.channel.send(animeInfoEmbed);
+                            console.log(msgArgs);
+                        });
+                    })
+                    break;
+
+                case 'mecha':
+                    https.get('https://api.jikan.moe/v3/search/anime?genre=18', res => {
+
+                        let body = '';
+
+                        res.on('data', chunk => {
+                            body += chunk;
+                        });
+
+                        res.on('end', () => {
+                            let bodyString = JSON.parse(body);
+                            console.log(bodyString.results[0].title);
+                            let topResult = bodyString.results[0];
+
+                            let animeInfoEmbed = new Discord.MessageEmbed()
+                                .setTitle("**Top search results for __Action__**")
+                                .setThumbnail(topResult.image_url)
+                                .setDescription(topResult.synopsis)
+                                .addField("Episodes:", topResult.episodes, true)
+                                .addField("Release Date:", topResult.start_date)
+
+
+                            message.channel.send(animeInfoEmbed);
+                            console.log(msgArgs);
+                        });
+                    })
+                    break;
+
+                case 'romance':
+                    https.get('https://api.jikan.moe/v3/search/anime?genre=22', res => {
+
+                        let body = '';
+
+                        res.on('data', chunk => {
+                            body += chunk;
+                        });
+
+                        res.on('end', () => {
+                            let bodyString = JSON.parse(body);
+                            console.log(bodyString.results[0].title);
+                            let topResult = bodyString.results[0];
+
+                            let animeInfoEmbed = new Discord.MessageEmbed()
+                                .setTitle("**Top search results for __Action__**")
+                                .setThumbnail(topResult.image_url)
+                                .setDescription(topResult.synopsis)
+                                .addField("Episodes:", topResult.episodes, true)
+                                .addField("Release Date:", topResult.start_date)
+
+
+                            message.channel.send(animeInfoEmbed);
+                            console.log(msgArgs);
+                        });
+                    })
+                    break;
+
+                case 'school':
+                    https.get('https://api.jikan.moe/v3/search/anime?genre=23', res => {
+
+                        let body = '';
+
+                        res.on('data', chunk => {
+                            body += chunk;
+                        });
+
+                        res.on('end', () => {
+                            let bodyString = JSON.parse(body);
+                            console.log(bodyString.results[0].title);
+                            let topResult = bodyString.results[0];
+
+                            let animeInfoEmbed = new Discord.MessageEmbed()
+                                .setTitle("**Top search results for __Action__**")
+                                .setThumbnail(topResult.image_url)
+                                .setDescription(topResult.synopsis)
+                                .addField("Episodes:", topResult.episodes, true)
+                                .addField("Release Date:", topResult.start_date)
+
+
+                            message.channel.send(animeInfoEmbed);
+                            console.log(msgArgs);
+                        });
+                    })
+                    break;
+
+                case 'shounen':
+                    https.get('https://api.jikan.moe/v3/search/anime?genre=27', res => {
+
+                        let body = '';
+
+                        res.on('data', chunk => {
+                            body += chunk;
+                        });
+
+                        res.on('end', () => {
+                            let bodyString = JSON.parse(body);
+                            console.log(bodyString.results[0].title);
+                            let topResult = bodyString.results[0];
+
+                            let animeInfoEmbed = new Discord.MessageEmbed()
+                                .setTitle("**Top search results for __Action__**")
+                                .setThumbnail(topResult.image_url)
+                                .setDescription("**" + topResult.title + "** \n" + topResult.synopsis)
+                                .addField("Episodes:", topResult.episodes, true)
+                                .addField("Release Date:", topResult.start_date)
+
+
+                            message.channel.send(animeInfoEmbed);
+                            console.log(msgArgs);
+                        });
+                    })
+                    break;
+            }
+        }
+
+
 
     }
 
--- commands/help.js
+++ commands/help.js
@@ -19,8 +19,8 @@ module.exports = {
             .addField("**%password**", "`generates a random password and sends it to you`", true)
             .addField("**%convert**", "`Allows you to provide a direct link to an image and convert it to the given file type` \n\n example: %convert png https://cdn.discordapp.com/attachments/583683143895220226/761889131034574858/20201002_101811.jpg", true)
             .addField("**%welcome**", "`If the user of the command is admin it toggles the welcoming message`", true)
-            .addField("**%findanime <name>**", "`Will show you some information about the given anime`", true)
-            .setFooter("__Also check out the bot\'s website:  https://Nofu-Bot.totnofu.repl.co__")
+            .addField("**%findanime <name>**", "`Will show you some information about the given anime // or will search by the given genre (only one word for example \"action\"; \ndo %searchinfo to see all genres)`", true)
+            .setFooter("Also check out the bot\'s website:  https://Nofu-Bot.totnofu.repl.co")
         message.channel.send(embed).catch(error => {
             console.error("Es ist folgender Fehler in help.js aufgetreten:", error);
         });
