e8b8c0ac03db1143f6213c6f5a2f4fb9cbb535f4
Zedruc <69540929+Zedruc@users.noreply.github.com>
2020-10-06 17:14:35 +0200

    Create findsongs.js

---
+++ commands/findsongs.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const https = require('https');
+
+module.exports = {
+    name: 'findsongs',
+    descriptioN: 'searches songs of given artist',
+    execute(message, args) {
+        let url = "https://itunes.apple.com/search?term=";
+        let body = '';
+        let msgArgs = message.content.slice(prefix.length).trim().split(/ +/);
+        let term = message.content.slice(prefix.length + 9).trim().split(/ +/);
+
+        https.get(url + term, res => {
+            res.on('data', chunk => {
+                body += chunk;
+            })
+
+            res.on('end', () => {
+                let bodyString = JSON.parse(body);
+                console.log(bodyString);
+
+                let bodyString = JSON.parse(body);
+                console.log(bodyString);
+
+                let length = bodyString.results.length - 1;
+                let allNames = [];
+                let i = 0;
+                do {
+                    i++
+                    allNames.push(bodyString.results[i].trackName);
+                } while (i < length);
+
+                let songsEmbed = new Discord.MessageEmbed()
+                    .setTitle("**Songs from: __" + term)
+                    .setDescription(allNames);
+
+                message.reply(songsEmbed);
+            })
+        })
+    }
+}
