36e4fed6267877fc9092a19fc1f39efd1070e58a
Zedruc <69540929+Zedruc@users.noreply.github.com>
2020-10-07 15:36:49 +0200

    Update findsongs.js

--- commands/findsongs.js
+++ commands/findsongs.js
@@ -31,7 +31,7 @@ module.exports = {
                 } while (i < length);
 
                 let songsEmbed = new Discord.MessageEmbed()
-                    .setTitle("**Songs from: __" + term + "__**")
+                    .setTitle("**Songs from: __" + term.replace(/,/g, " ") + "__**")
                     .setDescription(allNames + "\n\n" + "Go to the artist on Itunes here: " + bodyString.results[0].artistViewUrl)
                     .setThumbnail(bodyString.results[0].artworkUrl100)
 
