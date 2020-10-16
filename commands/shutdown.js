f3d534503bd3342bbc6d49f82ecab33149fbe057
Zedruc <69540929+Zedruc@users.noreply.github.com>
2020-10-17 01:29:18 +0200

    Update shutdown.js

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -7,32 +7,7 @@ module.exports = {
     name: "client",
     description: "will destroy the client",
     execute(message, args){
-        let prefix = "%";
-        let msgArgs = message.content.slice(prefix.length).trim().split(/ +/);
-        
-        if(message.author.id !== owner){
-            message.reply("You have NO permissions to use owner commands! Attempt will be logged")
-            return;
-        } else {
-            if(msgArgs[1] == "shutdown"){
-                let embed = new Discord.MessageEmbed()
-                .setTitle("**__Shutting down and sestroying client...__**")
-                .setDescription("Will boot on console-command")
-                .setColor('#ff0000');
-                message,channel.send(embed);
-                client.destroy()
-            } else if(msgArgs[1] == "restart"){
-                 let embed = new Discord.MessageEmbed()
-                .setTitle("**__Restarting client...__**")
-                .setDescription("Will boot up automatically")
-                .setColor('#ff0000');
-     
-                message.channel.send(embed)
-                return client.destroy()
-                       .then(() => {
-                       client.login(process.env.token)
-                });
-            }
+          console.log("shutdown logic in main");
         }
     }
 }
