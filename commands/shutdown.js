9656ee4d0ae0fb80e115ee868e27e8fb18323500
Zedruc <69540929+Zedruc@users.noreply.github.com>
2020-10-17 01:03:15 +0200

    Create shutdown.js

---
+++ commands/shutdown.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "client",
+    description: "will destroy the client",
+    execute(message, args){
+        let prefix = "%";
+        let msgArgs = message.content.slice(prefix.length).trim().split(/ +/);
+        
+        if(message.author.id !== 568729687291985930){
+            return;
+        } else {
+            if(msgArgs[1] == "shutdown"){
+                let embed = new Discord.messageEmbed()
+                .setTitle("**__Shutting down and sestroying client...__**")
+                .setDescription("Will boot on console-command");
+                message,channel.send(embed);
+                client.destroy()
+            } else if(msgArgs[1] == "restart"){
+                 let embed = new Discord.messageEmbed()
+                .setTitle("**__Restarting client...__**")
+                .setDescription("Will boot up automatically");
+                message.channel.send(embed);
+                client.destroy().then(() => {
+                    client.login(process.env.token)
+                })
+            }
+        }
+    }
+}
