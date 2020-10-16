2265888b3780b3f8fd24019c11fb71bdd492961a
Zedruc <schmidtmarkus727@gmail.com>
2020-10-16 15:43:47 +0200

    testing play command

--- commands/joinVc.js
+++ commands/joinVc.js
@@ -2,17 +2,55 @@
  * This is just a test
  * You wont see this in the %help command
  */
-
 const Discord = require("discord.js");
+const ytdl = require("ytdl-core");
 
 module.exports = {
     name: "joinvc",
     description: "makes the bot join your vc",
     execute(message, args) {
 
-        if (message.guild === null) return;
+        function play(connection, message) {
+            let server = servers[message.guild.id];
 
-        let channel = message.member.voice.channel;
-        channel.join;
+            server.dispatcher = connection.play(ytdl(server.queue[0], { filter: "audioonly" }));
+
+            server.queue.shift();
+
+            server.dispatcher.on('end', function () {
+                if (server.queue[0]) {
+                    play(connection, message);
+                } else {
+                    connection.disconnect();
+                }
+            });
+
+        }
+
+        let servers = {};
+
+        if (!args[1]) {
+            message.reply('Please provide a link!');
+            return;
+        }
+
+        if (!message.member.voiceChannel) {
+            message.reply('You must be in a voice channel!');
+            return;
+        }
+
+        if (!servers[message.guild.id]) servers[message.guild.id] = {
+            queue: []
+        }
+
+        let server = server[message.guild.id];
+
+        server.queue.push(args[1]);
+
+        if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function (connection) {
+            play(connection, message);
+        })
+
+
     }
 }
