a40297bccced89570562da9e557929e837810861
Zedruc <69540929+Zedruc@users.noreply.github.com>
2020-10-18 23:54:11 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -3,8 +3,8 @@ module.exports = {
         description: "bans user",
         execute(message, args) {
             // Ban a user by ID (or with a user/guild member object)
-            (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You need the Ban Members permission.")
-            if (!message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])) return message.channel.send("You need to provide a valid user.")
+            (!message.member.hasPermission("BAN_MEMBERS")) message.channel.send("You need the Ban Members permission.")
+            if (!message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])) message.channel.send("You need to provide a valid user.")
 
             guild.members.ban(message.mentions.users.first().id || message.guild.members.get(args[0])
                 .then(user => console.log(`Banned ${user.username || user.id || user} from ${guild.name}`))
