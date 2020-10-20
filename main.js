6557e6bd9b1d0d202bcd54f2ceee57a525f070d5
Zedruc <schmidtmarkus727@gmail.com>
2020-10-20 18:47:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -153,53 +153,7 @@ client.on('message', message => {
 
     } else if (command == 'hug') {
 
-        //client.commands.get('hug').execute(message, args);
-
-        if (message.guild === null) return;
-
-        const taggedUser = message.mentions.users.first();
-
-        let prefix = "%";
-
-        let key = process.env.tenorkey;
-        let msgArgs = message.content.slice(prefix.length).trim().split(/ +/);
-
-
-
-        https.get('https://api.tenor.com/v1/search?q=anime+hug&limit=50&key=' + key, res => {
-
-            let body = '';
-
-            res.on('data', chunk => {
-                body += chunk;
-            })
-
-            res.on('end', () => {
-                let bodyString = JSON.parse(body);
-                let int = Math.floor(Math.random(1) * 49);
-
-                let gifUrl = bodyString.results[int].media[0].tinygif.url;
-
-                if (taggedUser == message.author.id) {
-
-                    message.channel.send("You must be lonely huh")
-
-                } else if (taggedUser == 760905298990202901) {
-
-                    message.reply("Wait what?");
-
-                } else {
-
-                    let hugEmbed = new Discord.MessageEmbed()
-                        .setColor('#9E1A1A')
-                        .setTitle(message.member.displayName + " hugged " + `<@${taggedUser}>` + "! \n")
-                        .setAuthor(message.member.displayName + " hugged " + msgArgs[1] + "! \n")
-                        .setImage(gifUrl)
-                    message.channel.send(hugEmbed)
-
-                }
-            })
-        })
+        client.commands.get('hug').execute(message, args);
 
     } else if (command == 'ban') {
 
