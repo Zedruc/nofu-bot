3ebe604e5477846d88770a29e3536362bde0fda9
Zedruc <schmidtmarkus727@gmail.com>
2020-10-20 19:02:46 +0200

    Update broadcast.js

--- commands/broadcast.js
+++ commands/broadcast.js
@@ -8,30 +8,8 @@ module.exports = {
         const prefix = "%";
         var messageArgs = message.content.slice(prefix.length + 9).trim().split(/ +/);
         if (message.author.id === "568729687291985930") {
-            try {
-                let toSay = "test broadcast! _If this worked ima go crazy_"
-                this.client.guilds.map((guild) => {
-                    let found = 0
-                    guild.channels.map((c) => {
-                        if (found === 0) {
-                            if (c.type === "text") {
-                                if (c.permissionsFor(this.client.user).has("VIEW_CHANNEL") === true) {
-                                    if (c.permissionsFor(this.client.user).has("SEND_MESSAGES") === true) {
-                                        c.send(toSay);
-                                        found = 1;
-                                    }
-                                }
-                            }
-                        }
-                    });
-                });
-            }
-            catch (err) {
-                console.log("Could not send message to a (few) guild(s)!");
-                message.channel.send("`" + err + "`");
-            }
-        } else {
-            message.reply("You cant do that!")
+            let guilds = client.guilds.cache.filter(g => g.memberCount > 15);
+            guilds.send("siis");
         }
     }
 }
