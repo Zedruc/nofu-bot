91367d0db7dd918912df68c2605a1be44855521d
Zedruc <schmidtmarkus727@gmail.com>
2020-10-21 19:47:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ client.on("ready", () => {
     setInterval(() => {
 
         let defaultChannel = "";
-        client.guild.channels.cache.forEach((channel) => {
+        client.guilds.channels.cache.forEach((channel) => {
             if (channel.type == "text" && defaultChannel == "") {
                 if (channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
                     defaultChannel = channel;
