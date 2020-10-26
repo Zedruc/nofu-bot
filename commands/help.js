f041bf534e2d4891407352f1fe4e256b26d2c4d0
Zedruc <schmidtmarkus727@gmail.com>
2020-10-26 19:20:55 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -27,7 +27,7 @@ module.exports = {
             .addField("**%punch @user**", "`b o n k`")
             .addField("**%hug @user**", "`Hug someone :>`")
             .addField("**%stare** @user", "Intense  s t a r i n g")
-            .addField("**%devyt**", "`Shows live-information about my YouTube channel hehe :p (e.g. Is Libve: false/true)`")
+            .addField("**%devyt**", "`Shows live-information about my YouTube channel hehe :p (e.g. Is Live: false/true)`")
             .setFooter("Also check out the bot\'s website:  https://Nofu-Bot.totnofu.repl.co")
         message.channel.send(embed).catch(error => {
             console.error("Es ist folgender Fehler in help.js aufgetreten:", error);
