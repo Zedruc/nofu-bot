b82784505442f6e9e3d31bb82fc9db9525d4110a
Zedruc <schmidtmarkus727@gmail.com>
2020-10-31 16:33:55 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -6,7 +6,7 @@ module.exports = {
   execute(message, args) {
     // Ban a user by ID (or with a user/guild member object)
     if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You dont have the permission to run this command")
-    let User = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+    let User = message.guild.member(message.mentions.users.first()) || message.guild.members.cache.get(args[0])
     if (!User) return message.channel.send("No user given to ban")
     if (User.hasPermission("BAN_MEMBERS")) return message.reply("Can't kick user since their role is equal to or above mine")
     let banReason = args.join(" ").slice(22);
