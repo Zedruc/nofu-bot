9bb301a892b14983ffc83926176bf768512d1ddb
Zedruc <schmidtmarkus727@gmail.com>
2020-10-31 23:47:39 +0100

    Added some info

--- commands/mcstats.js
+++ commands/mcstats.js
@@ -16,8 +16,6 @@ module.exports = {
 
             res.on('end', () => {
                 let bodyString = JSON.parse(body);
-                let test = body.toString();
-                console.log(test);
                 console.log(bodyString);
                 let sender = message.author.tag;
                 let user = bodyString.regs[sender];
@@ -28,48 +26,31 @@ module.exports = {
                     return;
                 }
 
-                MojangAPI.nameHistory(UUID, function (err, res) {
-                    if (err)
-                        console.log(err);
-                    else {
-                        if (res.length == 1) {
-                            message.reply(res[0].name + " is very content with their existing username, because they didn't change it. Excellent job.");
-                        } else {
-                            var lastChange = res[res.length - 1];
-                            var at = new Date(lastChange.changedToAt);
-                            message.reply(lastChange.name + " wasn't so content with their username. They last changed their username at " + at + ".");
-                        }
-                    }
-                });
-
             })
-        }
-            //
-            //        https.get('https://api.mojang.com/user/profiles/' + UUID + '/names', res => {
-            //            let body = '';
-            //            res.on('data', chunk => {
-            //                body += chunk;
-            //
-            //                /**Error:
-            //                 * <!DOCTYPE html>
-            //                 * ^
-            //                 * SyntaxError: Unexpected token < in JSON at position 0
-            //                 * at JSON.parse (<anonymous>)
-            //                 * 
-            //                 * still todo
-            //                 * */
-            //
-            //            });
-            //
-            //            res.on('end', () => {
-            //                console.log(body[0]);
-            //                let a = body.replace(/ \[/g, "");
-            //                let b = console.log(a.replace(/ \]/g, ""));
-            //            });
-            //        });
-            //
-            //    });
-            //});
-        )
+        });
+
+        https.get('https://api.mojang.com/user/profiles/' + UUID + '/names', res => {
+            let body = '';
+
+            res.on('data', chunk => {
+                body += chunk;
+
+                /**Error:
+                 * <!DOCTYPE html>
+                 * ^
+                 * SyntaxError: Unexpected token < in JSON at position 0
+                 * at JSON.parse (<anonymous>)
+                 * 
+                 * still todo
+                 * */
+
+            });
+
+            res.on('end', () => {
+                console.log(body[0]);
+                let a = body.replace(/ \[/g, "");
+                let b = console.log(a.replace(/ \]/g, ""));
+            });
+        });
     }
 }
