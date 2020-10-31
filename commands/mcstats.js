d6be5abd57fdbdb32b66ae2622efc538bd008a46
Zedruc <schmidtmarkus727@gmail.com>
2020-10-31 17:12:49 +0100

    Update mcstats.js

--- commands/mcstats.js
+++ commands/mcstats.js
@@ -1,5 +1,6 @@
 const { SSL_OP_COOKIE_EXCHANGE } = require('constants');
 const https = require('https');
+const MojangAPI = require('mojang-api');
 
 module.exports = {
     name: 'mcstats',
@@ -25,30 +26,49 @@ module.exports = {
                     return;
                 }
 
-                https.get('https://api.mojang.com/user/profiles/' + UUID + '/names', res => {
-                    let body = '';
-                    res.on('data', chunk => {
-                        body += chunk;
-
-                        /**Error:
-                         * <!DOCTYPE html>
-                         * ^
-                         * SyntaxError: Unexpected token < in JSON at position 0
-                         * at JSON.parse (<anonymous>)
-                         * 
-                         * still todo
-                         * */
-
-                    });
-
-                    res.on('end', () => {
-                        console.log(body[0]);
-                        let a = body.replace(/ \[/g, "");
-                        let b = console.log(a.replace(/ \]/g, ""));
-                    });
+                MojangAPI.nameHistory(UUID, function (err, res) {
+                    if (err)
+                        console.log(err);
+                    else {
+                        if (res.length == 1) {
+                            message.reply(res[0].name + " is very content with their existing username, because they didn't change it. Excellent job.");
+                        } else {
+                            var lastChange = res[res.length - 1];
+                            var at = new Date(lastChange.changedToAt);
+                            message.reply(lastChange.name + " wasn't so content with their username. They last changed their username at " + at + ".");
+                        }
+                    }
                 });
 
-            });
-        });
-    }
+            })
+        }
+            //
+            //        https.get('https://api.mojang.com/user/profiles/' + UUID + '/names', res => {
+            //            let body = '';
+            //            res.on('data', chunk => {
+            //                body += chunk;
+            //
+            //                /**Error:
+            //                 * <!DOCTYPE html>
+            //                 * ^
+            //                 * SyntaxError: Unexpected token < in JSON at position 0
+            //                 * at JSON.parse (<anonymous>)
+            //                 * 
+            //                 * still todo
+            //                 * */
+            //
+            //            });
+            //
+            //            res.on('end', () => {
+            //                console.log(body[0]);
+            //                let a = body.replace(/ \[/g, "");
+            //                let b = console.log(a.replace(/ \]/g, ""));
+            //            });
+            //        });
+            //
+            //    });
+            //});
+        )
+    })
+}
 }
