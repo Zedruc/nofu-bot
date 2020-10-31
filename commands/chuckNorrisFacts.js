e4f9ef10930901515debb7720c6ab5b30fd24aee
George Ober <george.a.ober@gmail.com>
2020-10-31 08:41:24 +0100

    Chuck norris facts update

--- commands/chuckNorrisFacts.js
+++ commands/chuckNorrisFacts.js
@@ -3,13 +3,10 @@ module.exports = {
   name: 'chuckNorrisFacts',
   description: "Sends a random Chuck Norris fact from the CNfDB",
   async execute(message, args) {
-    //Sends a request to the icndb for a joke. I should probably exclude the explicit ones but I don't really care atm
-    let x = await fetch("http://api.icndb.com/jokes/random?exclude=[explicit]");
+    // Sends a request to the icndb for a joke, excluding the explicit jokes.
+    let x = await fetch("http://api.icndb.com/jokes/random?exclude=[explicit]&escape=javascript");
     x.json().then(body => {
       message.channel.send(body.value.joke);
     });
-    //By the actuall way, sometimes, the joke sent by the API contains something like "&quot;"
-    //which is the special HTML character for ". As I am not sure it does it for other things,
-    //I will probably add a String.prototype.replace("&quot;", '"')
   }
 }
