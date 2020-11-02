4fd0a59d69f940bfba72d267afd21db87eb93115
Zedruc <schmidtmarkus727@gmail.com>
2020-11-02 20:09:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -24,170 +24,173 @@ client.on("ready", () => {
     console.info("Der Nofu-bot ist jetzt online!");
     client.user.setActivity('his master', { type: 'LISTENING' })
 
-    //setInterval(() => {
-    //
-    //  let defaultChannel = "";
-    //  client.guilds.channels.cache.forEach((channel) => {
-    //      if (channel.type == "text" && defaultChannel == "") {
-    //          if (channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
-    //                defaultChannel = channel;
-    //                defaultChannel.send("Don't forget to use `%idea` if you got an idea to improve the bot :D");
-    //         }
-    //        }
-    //    })
-    //}, 600000);
+    setInterval(() => {
 
-    http.get('http://worldtimeapi.org/api/timezone/Europe/Berlin',
-        res => {
+        //client.guilds.cache.forEach((guild) => { //for each guild the bot is in
+        //    let defaultChannel = "";
+        //    const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
+        //    if (!channel) {
+        //        guild.channels.cache.forEach((channel) => {
+        //            if (channel.type == "text" && defaultChannel == "") {
+        //                if (channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+        //                    defaultChannel = channel;
+        //                }
+        //            }
+        //        });
+        //    }
+        //}, 600000);
 
-            let data = '';
+        http.get('http://worldtimeapi.org/api/timezone/Europe/Berlin',
+            res => {
 
-            res.on('data', chunk => {
-                data += chunk;
-            })
+                let data = '';
 
-            res.on('end', () => {
-                let answer = JSON.parse(data);
-                console.info("Gestartet um: " + answer.datetime + "// Zeitzone: Europe/Berlin");
-            })
-        });
-});
+                res.on('data', chunk => {
+                    data += chunk;
+                })
 
-client.on('guildDelete', guild => {
-    console.log(`Bot wurde entfernt von ${guild.name} (id: ${guild.id})`)
-});
+                res.on('end', () => {
+                    let answer = JSON.parse(data);
+                    console.info("Gestartet um: " + answer.datetime + "// Zeitzone: Europe/Berlin");
+                })
+            });
+    });
 
-client.on("guildCreate", guild => {
-    console.log(`Wurde hinzugefügt ${guild.name} \n ${guild.id}`);
-});
+    client.on('guildDelete', guild => {
+        console.log(`Bot wurde entfernt von ${guild.name} (id: ${guild.id})`)
+    });
 
+    client.on("guildCreate", guild => {
+        console.log(`Wurde hinzugefügt ${guild.name} \n ${guild.id}`);
+    });
 
 
-client.on('message', message => {
 
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    client.on('message', message => {
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+        if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    if (command === 'ping') {
+        const args = message.content.slice(prefix.length).split(/ +/);
+        const command = args.shift().toLowerCase();
 
-        client.commands.get('ping').execute(message, args);
+        if (command === 'ping') {
 
-    } else if (command === "meme") {
+            client.commands.get('ping').execute(message, args);
 
-        client.commands.get('meme').execute(message, args);
+        } else if (command === "meme") {
 
-    } else if (command === 'help') {
+            client.commands.get('meme').execute(message, args);
 
-        client.commands.get('help').execute(message, args, client);
+        } else if (command === 'help') {
 
-    } else if (command === 'delete') {
+            client.commands.get('help').execute(message, args, client);
 
-        client.commands.get('delete').execute(message, args);
+        } else if (command === 'delete') {
 
-    } else if (command === 'serverinfo') {
+            client.commands.get('delete').execute(message, args);
 
-        client.commands.get('serverinfo').execute(message, args);
+        } else if (command === 'serverinfo') {
 
-    } else if (command === 'userinfo') {
+            client.commands.get('serverinfo').execute(message, args);
 
-        client.commands.get('userinfo').execute(message, args, client);
+        } else if (command === 'userinfo') {
 
-    } else if (command === 'joke') {
+            client.commands.get('userinfo').execute(message, args, client);
 
-        client.commands.get('joke').execute(message, args, client);
+        } else if (command === 'joke') {
 
-    } else if (command === 'uptime') {
+            client.commands.get('joke').execute(message, args, client);
 
-        let time = Date.now() - date_ob;
-        let seconds = time / 1000;
-        let hours = seconds / 3600;
-        let days = seconds / 86400;
+        } else if (command === 'uptime') {
 
-        let ps = seconds.toFixed();
-        let ph = hours.toFixed();
-        let pd = days.toFixed();
+            let time = Date.now() - date_ob;
+            let seconds = time / 1000;
+            let hours = seconds / 3600;
+            let days = seconds / 86400;
 
-        const statEmbed = new Discord.MessageEmbed()
-            .setTitle("**  = STATISTICS =**")
-            .addField("**Memory usage **", `**${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB**`)
-            .addField("**Uptime **", `${pd} days \n ${ph} hours \n ${ps} seconds`);
-        message.channel.send(statEmbed);
+            let ps = seconds.toFixed();
+            let ph = hours.toFixed();
+            let pd = days.toFixed();
 
-    } else if (command === 'mememan') {
+            const statEmbed = new Discord.MessageEmbed()
+                .setTitle("**  = STATISTICS =**")
+                .addField("**Memory usage **", `**${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB**`)
+                .addField("**Uptime **", `${pd} days \n ${ph} hours \n ${ps} seconds`);
+            message.channel.send(statEmbed);
 
-        client.commands.get('mememan').execute(message, args);
+        } else if (command === 'mememan') {
 
-    } else if (command === 'password') {
+            client.commands.get('mememan').execute(message, args);
 
-        client.commands.get('password').execute(message, args);
+        } else if (command === 'password') {
 
-    } else if (command === 'convert') {
+            client.commands.get('password').execute(message, args);
 
-        client.commands.get('convert').execute(message, args, client);
+        } else if (command === 'convert') {
 
-    } else if (command === 'findanime') {
+            client.commands.get('convert').execute(message, args, client);
 
-        client.commands.get('findanime').execute(message, args, client);
+        } else if (command === 'findanime') {
 
-    } else if (command == 'findsongs') {
+            client.commands.get('findanime').execute(message, args, client);
 
-        client.commands.get('findsongs').execute(message, args, client);
+        } else if (command == 'findsongs') {
 
-    } else if (command == 'punch') {
+            client.commands.get('findsongs').execute(message, args, client);
 
-        client.commands.get('punch').execute(message, args);
+        } else if (command == 'punch') {
 
-    } else if (command == 'client') {
+            client.commands.get('punch').execute(message, args);
 
-        client.commands.get('client').execute(message, args);
+        } else if (command == 'client') {
 
-    } else if (command == 'hug') {
+            client.commands.get('client').execute(message, args);
 
-        client.commands.get('hug').execute(message, args, client);
+        } else if (command == 'hug') {
 
-    } else if (command == 'ban') {
+            client.commands.get('hug').execute(message, args, client);
 
-        client.commands.get('ban').execute(message, args);
+        } else if (command == 'ban') {
 
-    } else if (command == 'kill') {
+            client.commands.get('ban').execute(message, args);
 
-        client.commands.get('kill').execute(message, args, client);
+        } else if (command == 'kill') {
 
-    } else if (command == "devyt") {
+            client.commands.get('kill').execute(message, args, client);
 
-        client.commands.get('devyt').execute(message, args, client);
+        } else if (command == "devyt") {
 
-    } else if (command == "stare") {
+            client.commands.get('devyt').execute(message, args, client);
 
-        client.commands.get('stare').execute(message, args);
+        } else if (command == "stare") {
 
-    } else if (command == "mcregister") {
+            client.commands.get('stare').execute(message, args);
 
-        client.commands.get("mcregister").execute(message, args);
+        } else if (command == "mcregister") {
 
-    } else if (command == "cnf") {
+            client.commands.get("mcregister").execute(message, args);
 
-        client.commands.get("chuckNorrisFacts").execute(message, args, client);
+        } else if (command == "cnf") {
 
-    } else if (command == "delete") {
+            client.commands.get("chuckNorrisFacts").execute(message, args, client);
 
-        client.commands.get("delete").execute(message, args);
+        } else if (command == "delete") {
 
-    } else if (command == "broadcast") {
+            client.commands.get("delete").execute(message, args);
 
-        client.commands.get("broadcast").execute(message, args, client);
+        } else if (command == "broadcast") {
 
-    } else if (command == "mcstats") {
+            client.commands.get("broadcast").execute(message, args, client);
 
-        client.commands.get("mcstats").execute(message, args);
+        } else if (command == "mcstats") {
 
-    } else if (command == "admin") {
+            client.commands.get("mcstats").execute(message, args);
 
-        client.commands.get("admin").execute(message, args);
+        } else if (command == "admin") {
 
-    }
-});
+            client.commands.get("admin").execute(message, args);
 
-client.login(process.env.token);
+        }
+    });
+
+    client.login(process.env.token);
