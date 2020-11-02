7054282ff35a502fd2309dcddcaf2309c267365f
Zedruc <schmidtmarkus727@gmail.com>
2020-11-02 13:52:04 +0100

    Update mcstats.js

--- commands/mcstats.js
+++ commands/mcstats.js
@@ -20,49 +20,49 @@ module.exports = {
                 console.log(bodyString);
                 let sender = message.author.tag;
                 let user = bodyString.regs[sender];
-                let UUID = user[0];
+                UUID = user[0];
                 console.log(UUID);
                 if (user == null) {
                     message.reply("You first have to login to your account via uuid!\n`%mcregister <UUID>`");
                     return;
                 }
 
+                https.get('https://api.mojang.com/user/profiles/' + UUID + '/names', res => {
+                    let body = '';
+
+                    res.on('data', chunk => {
+                        body += chunk;
+
+                    });
+
+                    res.on('end', () => {
+                        let bodyString = JSON.parse(body);
+                        console.log(bodyString);
+                        console.log(bodyString[0].name);
+                        let nOn /*number of names*/ = bodyString.length;
+
+                        let nameHistory = [];
+
+                        let mcStatsEmbed = new Discord.MessageEmbed()
+                            .setColor('#00ad00')
+                            .setTitle(sender.name + "\'s Minecraft name history")
+                            .setAuthor(sender.displayName, sender.displayAvatarURL, sender.displayAvatarURL)
+                            .setDescription(readableArr)
+
+
+                        for (let i = 0; i < bodyString.length; i++) {
+                            console.log(bodyString[i].name);
+                            nameHistory.push(bodyString[i].name)
+                            mcStatsEmbed
+                                .addField(i !== 0 ? new Date(bodyString[i].changedToAt) : "Original name: ", bodyString[i].name, true);
+                        }
+                        console.log("Minecraft name history of " + message.member.displayName + "\n" + nameHistory);
+                        let readableArr = nameHistory.join(" ");
+                        message.channel.send(mcStatsEmbed);
+                    });
+                });
+
             })
-        });
-
-        https.get('https://api.mojang.com/user/profiles/' + UUID + '/names', res => {
-            let body = '';
-
-            res.on('data', chunk => {
-                body += chunk;
-
-            });
-
-            res.on('end', () => {
-                let bodyString = JSON.parse(body);
-                console.log(bodyString);
-                console.log(bodyString[0].name);
-                let nOn /*number of names*/ = bodyString.length;
-
-                let nameHistory = [];
-
-                let mcStatsEmbed = new Discord.MessageEmbed()
-                    .setColor('#00ad00')
-                    .setTitle(sender.name + "\'s Minecraft name history")
-                    .setAuthor(sender.displayName, sender.displayAvatarURL, sender.displayAvatarURL)
-                    .setDescription(readableArr)
-
-
-                for (let i = 0; i < bodyString.length; i++) {
-                    console.log(bodyString[i].name);
-                    nameHistory.push(bodyString[i].name)
-                    mcStatsEmbed
-                        .addField(i !== 0 ? new Date(bodyString[i].changedToAt) : "Original name: ", bodyString[i].name, true);
-                }
-                console.log("Minecraft name history of " + message.member.displayName + "\n" + nameHistory);
-                let readableArr = nameHistory.join(" ");
-                message.channel.send(mcStatsEmbed);
-            });
         });
     }
 }
