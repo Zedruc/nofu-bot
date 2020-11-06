f363213400d7d7275336d4eeb9299843e9ca2b9f
Zedruc <schmidtmarkus727@gmail.com>
2020-11-06 19:22:16 +0100

    Update pokéInfo.js

--- commands/pokéInfo.js
+++ commands/pokéInfo.js
@@ -23,21 +23,21 @@ module.exports = {
                 let abilitypath = bodyString.abilities;
 
                 let pokéInfoEmbed = new Discord.MessageEmbed()
-                    .setTitle("**__Pokédex |" + bodyString.name + "__**")
+                    .setTitle("**__Pokédex | " + bodyString.name + "__**")
                     .setImage('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + id + '.png')
                     .setFooter("More Info will be provided soon", client.user.displayAvatarURL({ format: "png" }))
                     .setTimestamp();
                 for (let i = 0; i < bodyString.abilities.length; i++) {
                     //All abilities, name, add a image from the pokémon
                     pokéInfoEmbed.addField(`__Ability ${i + 1}__`, abilitypath[i].ability.name, true)
-                    if (i++ > bodyString.abilities.length) {
+                    if (i == bodyString.abilities.length) {
                         break;
                     }
                 }
 
                 for (let i = 0; i < bodyString.forms.length; i++) {
                     pokéInfoEmbed.addField(`__Form ${i + 1}__`, bodyString.forms[i].name, true)
-                    if (i++ > bodyString.forms) {
+                    if (i == bodyString.forms) {
                         break;
                     }
                 }
