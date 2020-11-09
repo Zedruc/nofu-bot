38c9e403e008ea313d294a51a8e54031ffc8c373
Zedruc <schmidtmarkus727@gmail.com>
2020-11-09 17:30:43 +0100

    Update broadcast.js

--- commands/broadcast.js
+++ commands/broadcast.js
@@ -16,7 +16,7 @@ module.exports = {
 
         client.guilds.cache.forEach((guild) => { //for each guild the bot is in
             let defaultChannel = "";
-            guild.channels.forEach((channel) => {
+            guild.channels.cache.forEach((channel) => {
                 if (channel.type == "text" && defaultChannel == "") {
                     if (channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
                         defaultChannel = channel;
