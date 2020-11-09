cd3c28f7c6c3ac4e7ca6a0397513ae1f23d04c13
Zedruc <schmidtmarkus727@gmail.com>
2020-11-09 17:24:38 +0100

    Update broadcast.js

--- commands/broadcast.js
+++ commands/broadcast.js
@@ -13,27 +13,29 @@ module.exports = {
         let msg = argsString.replace(/,/g, " ");
         //
 
-        client.guilds.cache.forEach((guild) => { //for each guild the bot is in
+        if (!message.author.id == "568729687291985930") {
+            return message.reply("Only the Bot owner is able to use this command!")
+        }
+
+        let rawArgs = args;
+        let argsString = rawArgs.toString();
+        let msg = argsString.replace(/,/g, " ");
+        //
+
+        client.guilds.forEach((guild) => { //for each guild the bot is in
             let defaultChannel = "";
-            const channel = message.member.guild.channels.cache.find(ch => ch.name === 'general');
-            if (!channel) {
-                guild.channels.cache.forEach((channel) => {
-                    if (channel.type == "text" && defaultChannel == "") {
-                        if (channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
-                            defaultChannel = channel;
-                        }
+            guild.channels.forEach((channel) => {
+                if (channel.type == "text" && defaultChannel == "") {
+                    if (channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+                        defaultChannel = channel;
                     }
-                    let broadcastEmbed = new Discord.MessageEmbed()
-                        .setTitle("**__Broadcast__**")
-                        .setDescription(msg)
-                    defaultChannel.send(broadcastEmbed)
-                });
-            } else {
+                }
                 let broadcastEmbed = new Discord.MessageEmbed()
                     .setTitle("**__Broadcast__**")
                     .setDescription(msg)
-                channel.send(broadcastEmbed)
-            }
+                defaultChannel.send(broadcastEmbed)
+            });
+
         });
     }
 }
