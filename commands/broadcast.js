83435aaefd6c9be2e54a3469bb75c8e0cda70f01
Zedruc <schmidtmarkus727@gmail.com>
2020-11-09 17:11:52 +0100

    Update broadcast.js

--- commands/broadcast.js
+++ commands/broadcast.js
@@ -8,32 +8,55 @@ module.exports = {
             return;
         }
 
-        let rawArgs = args;
-        let argsString = rawArgs.toString();
-        let msg = argsString.replace(/,/g, " ");
+        //        let rawArgs = args;
+        //        let argsString = rawArgs.toString();
+        //        let msg = argsString.replace(/,/g, " ");
+        //        //
         //
-
-        client.guilds.cache.forEach((guild) => { //for each guild the bot is in
-            let defaultChannel = "";
-            const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
-            if (!channel) {
-                guild.channels.cache.forEach((channel) => {
-                    if (channel.type == "text" && defaultChannel == "") {
-                        if (channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
-                            defaultChannel = channel;
+        //        client.guilds.cache.forEach((guild) => { //for each guild the bot is in
+        //            let defaultChannel = "";
+        //            const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
+        //            if (!channel) {
+        //                guild.channels.cache.forEach((channel) => {
+        //                    if (channel.type == "text" && defaultChannel == "") {
+        //                        if (channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+        //                            defaultChannel = channel;
+        //                        }
+        //                    }
+        //                    let broadcastEmbed = new Discord.MessageEmbed()
+        //                        .setTitle("**__Broadcast__**")
+        //                        .setDescription(msg)
+        //                    defaultChannel.send(broadcastEmbed)
+        //                });
+        //            } else {
+        //                let broadcastEmbed = new Discord.MessageEmbed()
+        //                    .setTitle("**__Broadcast__**")
+        //                    .setDescription(msg)
+        //                channel.send(broadcastEmbed)
+        //            }
+        //        });
+        try {
+            let toSay = args.toString().replace(/,/g, " ");
+            this.client.guilds.map((guild) => {
+                let found = 0
+                guild.channels.map((c) => {
+                    if (found === 0) {
+                        if (c.type === "text") {
+                            if (c.permissionsFor(this.client.user).has("VIEW_CHANNEL") === true) {
+                                if (c.permissionsFor(this.client.user).has("SEND_MESSAGES") === true) {
+                                    c.send(toSay);
+                                    found = 1;
+                                }
+                            }
                         }
                     }
-                    let broadcastEmbed = new Discord.MessageEmbed()
-                        .setTitle("**__Broadcast__**")
-                        .setDescription(msg)
-                    defaultChannel.send(broadcastEmbed)
                 });
-            } else {
-                let broadcastEmbed = new Discord.MessageEmbed()
-                    .setTitle("**__Broadcast__**")
-                    .setDescription(msg)
-                channel.send(broadcastEmbed)
-            }
-        });
+            });
+        }
+        catch (err) {
+            console.log("Could not send message to a (few) guild(s)!");
+        }
+    } else {
+        message.reply("You cant do that!")
     }
 }
