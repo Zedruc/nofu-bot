8cda7b7866d4b2a79c93289f1c77009c6a60d213
Zedruc <schmidtmarkus727@gmail.com>
2020-12-08 18:58:09 +0100

    Update homeschoolingCommandsPrivate.js

--- commands/homeschoolingCommandsPrivate.js
+++ commands/homeschoolingCommandsPrivate.js
@@ -17,10 +17,10 @@ module.exports = {
                 .setThumbnail("https://img.icons8.com/ios/452/monday.png")
                 .addFields(
                     { name: "__8:00 - 8:45 / 1. Stunde__", value: "Mathe", inline: true },
-                    { name: "__8:45 - 9:30 / 2. Stunde__", value: "Englisch", inline: true },
+                    { name: "__8:45 - 9:30 / 2. Stunde__", value: "Englisch" },
                     { name: "__9:30 - 9:45 / Erste Pause__", value: "---------", inline: true },
                     { name: "__9:45 - 10:30 / 3. Stunde__", value: "Deutsch", inline: true },
-                    { name: "__10:30 - 11:15 / 4. Stunde__", value: "Deutsch", inline: true },
+                    { name: "__10:30 - 11:15 / 4. Stunde__", value: "Deutsch" },
                     { name: "__11:15 - 11:30 / Zweite Pause__", value: "---------", inline: true },
                     { name: "__11:30 - 12:15 / 5. Stunde__", value: "Geographie", inline: true },
                     { name: "__12:15 - 13:00 / 6. Stunde__", value: "Französisch", inline: true }
@@ -34,10 +34,10 @@ module.exports = {
                 .setThumbnail("https://img.icons8.com/ios/452/tuesday.png")
                 .addFields(
                     { name: "__8:00 - 8:45 / 1. Stunde__", value: "Religion", inline: true },
-                    { name: "__8:45 - 9:30 / 2. Stunde__", value: "Englisch", inline: true },
+                    { name: "__8:45 - 9:30 / 2. Stunde__", value: "Englisch" },
                     { name: "__9:30 - 9:45 / Erste Pause__", value: "---------", inline: true },
                     { name: "__9:45 - 10:30 / 3. Stunde__", value: "Geschichte", inline: true },
-                    { name: "__10:30 - 11:15 / 4. Stunde__", value: "Physik", inline: true },
+                    { name: "__10:30 - 11:15 / 4. Stunde__", value: "Physik" },
                     { name: "__11:15 - 11:30 / Zweite Pause__", value: "---------", inline: true },
                     { name: "__11:30 - 12:15 / 5. Stunde__", value: "Biologie", inline: true },
                     { name: "__12:15 - 13:00 / 6. Stunde__", value: "IT", inline: true }
@@ -51,10 +51,10 @@ module.exports = {
                 .setThumbnail("https://img.icons8.com/ios/452/wednesday.png")
                 .addFields(
                     { name: "__8:00 - 8:45 / 1. Stunde__", value: "Sport", inline: true },
-                    { name: "__8:45 - 9:30 / 2. Stunde__", value: "Sport", inline: true },
+                    { name: "__8:45 - 9:30 / 2. Stunde__", value: "Sport" },
                     { name: "__9:30 - 9:45 / Erste Pause__", value: "---------", inline: true },
                     { name: "__9:45 - 10:30 / 3. Stunde__", value: "Mathe", inline: true },
-                    { name: "__10:30 - 11:15 / 4. Stunde__", value: "Französisch", inline: true },
+                    { name: "__10:30 - 11:15 / 4. Stunde__", value: "Französisch" },
                     { name: "__11:15 - 11:30 / Zweite Pause__", value: "---------", inline: true },
                     { name: "__11:30 - 12:15 / 5. Stunde__", value: "Deutsch", inline: true },
                     { name: "__12:15 - 13:00 / 6. Stunde__", value: "Deutsch", inline: true }
@@ -68,10 +68,10 @@ module.exports = {
                 .setThumbnail("https://img.icons8.com/ios/452/thursday.png")
                 .addFields(
                     { name: "__8:00 - 8:45 / 1. Stunde__", value: "BWR", inline: true },
-                    { name: "__8:45 - 9:30 / 2. Stunde__", value: "Englisch", inline: true },
+                    { name: "__8:45 - 9:30 / 2. Stunde__", value: "Englisch" },
                     { name: "__9:30 - 9:45 / Erste Pause__", value: "---------", inline: true },
                     { name: "__9:45 - 10:30 / 3. Stunde__", value: "Biologie", inline: true },
-                    { name: "__10:30 - 11:15 / 4. Stunde__", value: "Geographie", inline: true },
+                    { name: "__10:30 - 11:15 / 4. Stunde__", value: "Geographie" },
                     { name: "__11:15 - 11:30 / Zweite Pause__", value: "---------", inline: true },
                     { name: "__11:30 - 12:15 / 5. Stunde__", value: "Religion", inline: true },
                     { name: "__12:15 - 13:00 / 6. Stunde__", value: "Mathe", inline: true }
@@ -85,10 +85,10 @@ module.exports = {
                 .setThumbnail("https://img.icons8.com/ios/452/friday.png")
                 .addFields(
                     { name: "__8:00 - 8:45 / 1. Stunde__", value: "Französisch", inline: true },
-                    { name: "__8:45 - 9:30 / 2. Stunde__", value: "IT", inline: true },
+                    { name: "__8:45 - 9:30 / 2. Stunde__", value: "IT" },
                     { name: "__9:30 - 9:45 / Erste Pause__", value: "---------", inline: true },
                     { name: "__9:45 - 10:30 / 3. Stunde__", value: "Geschichte", inline: true },
-                    { name: "__10:30 - 11:15 / 4. Stunde__", value: "Physik", inline: true },
+                    { name: "__10:30 - 11:15 / 4. Stunde__", value: "Physik" },
                     { name: "__11:15 - 11:30 / Zweite Pause__", value: "---------", inline: true },
                     { name: "__11:30 - 12:15 / 5. Stunde__", value: "BWR", inline: true },
                     { name: "__12:15 - 13:00 / 6. Stunde__", value: "Englisch", inline: true }
