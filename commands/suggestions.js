5c1e18af92a42967d3d513afc24f1d0e024407a1
Zedruc <schmidtmarkus727@gmail.com>
2021-02-13 20:07:56 +0100

    Update suggestions.js

--- commands/suggestions.js
+++ commands/suggestions.js
@@ -6,23 +6,22 @@ module.exports = {
     execute(message, args, client) {
         var suggestionContent = {};
         if (message.guild) return message.reply("please use this command in my direct messages!");
-        message.channel.send("What's your suggestion?");
-
         const filter = (m) => m.author.id === message.author.id
+        message.channel.send("What's your suggestion?");
         message.channel.awaitMessages(filter, { max: 1, time: 180000 })
             .then((collected) => {
                 suggestionContent["userID"] = collected.first().author.id;
                 suggestionContent["userName"] = collected.first().author;
                 suggestionContent["message"] = collected.first().content;
+
+                let suggestionEmbed = new Discord.MessageEmbed()
+                    .setTitle("**New Suggestion!**")
+                    .addField(`__Suggestion by: ${suggestionContent.userName} (${suggestionContent.userID})__`, `${suggestionContent.message}`)
+                client.users.get("568729687291985930").send(suggestionEmbed);
             }).catch((err) => {
                 message.channel.send("You took longer than 3 minutes!")
                 console.log(err);
             });
-
-        let suggestionEmbed = new Discord.MessageEmbed()
-            .setTitle("**New Suggestion!**")
-            .addField(`__Suggestion by: ${suggestionContent.userName} (${suggestionContent.userID})__`, `${suggestionContent.message}`)
-        client.users.get("568729687291985930").send(suggestionEmbed);
 
 
     }
