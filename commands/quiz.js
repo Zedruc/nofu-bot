759162fa31a3e5b27667c0ed23b00a252a0751ec
Zedruc <schmidtmarkus727@gmail.com>
2021-02-16 13:44:30 +0100

    Update quiz.js

--- commands/quiz.js
+++ commands/quiz.js
@@ -16,7 +16,7 @@
 // =============================================================================
 const Discord = require('discord.js');
 const fs = require('fs');
-const { path } = require('path');
+const path = require('path');
 
 const topics = [
     "geography",
