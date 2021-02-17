02760879f55aa7e30fef68e8c160462b0d153c86
Zedruc <schmidtmarkus727@gmail.com>
2021-02-17 16:27:33 +0100

    nvm

--- main.js
+++ main.js
@@ -60,6 +60,75 @@ client.on("guildCreate", guild => {
 
 client.on('message', message => {
 
+    // =============================================================================
+    // MODERATION BLOCK FOR SERVERS WHICH REQUESTED
+    // =============================================================================
+    if (message.guild.id == "704285475791437844") {
+
+        const slurs = [
+            "nigga",
+            "nigger",
+            "ching chong", // server owner chose this, idk why lul
+            "asshole",
+            "bitch",
+            "twat",
+            "slut",
+            "whore",
+            "hoe",
+            "cunt",
+            "blowjob",
+            "pussy",
+            "cock",
+            "dick",
+            "bussy",
+            "faggot",
+            "nut",
+            "cum",
+            "sloot",
+            "cont",
+            "puthay",
+            "peg",
+            "motherfucker",
+            "retard",
+            "tits",
+            "boobs"
+        ];
+
+        if (message.author.id == "701150875871346809") {
+            if (message.content.includes("%slurlist")) {
+                let response = slurs.join(", ");
+                message.author.send(response);
+            }
+        }
+
+        let text = message.content;
+        let stemmedText = stemmer.tokenizeAndStem(text, true);
+
+        for (let x = 0; x < slurs.length; x++) {
+
+            if (stemmedText.indexOf(slurs[x]) > -1) {
+                try {
+                    message.delete({ timeout: 200, reason: "Detected word in filer, autodeleted." });
+                    return;
+                } catch (err) {
+                    message.channel.send("Critical error while deleting message, shutting down.");
+                    throw new Error(err);
+                }
+            }
+
+        }
+
+    }
+
+    // =========================================================================
+    // END MODERATION BLOCK
+    // =========================================================================
+
+
+
+
+
+
     // =========================================================================
     // JUST FOR FUN
     // =========================================================================
@@ -104,8 +173,6 @@ client.on('message', message => {
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
-
-    client.commands.get("filter").execute(message);
 
     if (command === 'ping') {
         message.react("🏓");
