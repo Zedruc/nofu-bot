a087ff39e3937e6a776b5c89f257340e950eb54f
Zedruc <schmidtmarkus727@gmail.com>
2021-02-18 17:38:59 +0100

    Update oof.js

--- commands/oof.js
+++ commands/oof.js
@@ -10,7 +10,7 @@ module.exports = {
         vc.join().then(connection => {
             let dispatcher = connection.play("https://www.myinstants.com/media/sounds/roblox-death-sound_1.mp3") // lul
             dispatcher.on('finish', end => {
-                channel.send("oof");
+                message.react(":speaker:");
                 vc.leave();
             });
 
