8b9661b41c093709ad254dd616b3afef5cb90f0a
Zedruc <schmidtmarkus727@gmail.com>
2021-02-18 12:32:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -65,18 +65,20 @@ client.on("guildCreate", guild => {
 });
 
 client.on('guildMemberAdd', (member) => {
-    const staffChannel = client.channels.cache.find(channel => channel.id === "704372434022826136");
-    let today = new Date().getDate();
-    const newMember = member.user;
-    if (newMember.createdAt.getDate() == today) {
-        if (newMember.client.guilds.cache.size <= 6) {
-            let warning = new Discord.MessageEmbed()
-                .setTitle("[Developement Phase] __Potential alt account found__.")
-                .addFields(
-                    { name: `Potential alt account: ${newMember.discriminator}`, value: `Account created at ${newMember.createdAt.getDate()}` }
-                )
-                .setDescription("Remember that this detections aren't and never will be 100% correct")
-            staffChannel.send(warning);
+    if (member.guild.id == "704285475791437844") {
+        const staffChannel = client.channels.cache.find(channel => channel.id === "704372434022826136");
+        let today = new Date().getDate();
+        const newMember = member.user;
+        if (newMember.createdAt.getDate() == today) {
+            if (newMember.client.guilds.cache.size <= 6) {
+                let warning = new Discord.MessageEmbed()
+                    .setTitle("[Developement Phase] __Potential alt account found__.")
+                    .addFields(
+                        { name: `Potential alt account: ${newMember.discriminator}`, value: `Account created at ${newMember.createdAt.getDate()}` }
+                    )
+                    .setDescription("Remember that this detections aren't and never will be 100% correct")
+                staffChannel.send(warning);
+            }
         }
     }
 });
