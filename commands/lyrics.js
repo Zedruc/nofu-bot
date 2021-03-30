4a26bf38ac970e55c497a553aaa856924126ceb6
Nofu <69540929+Zedruc@users.noreply.github.com>
2021-03-30 14:45:32 +0200

    Update lyrics.js

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -4,10 +4,9 @@ module.exports = {
     name: 'lyrics',
     description: 'Searches for the lyrics of given song',
     execute(message, args, client) {
-        const api = "api.genius.com";
         let query = args.join(" ");
 
-        fetch(`${api}/search?q=${encodeURI(query)}`, {
+        fetch("api.genius.com/search?q=" + encodeURI(query), {
             method: 'GET',
             headers: {
                 Authorization: `Bearer ${process.env.genius_token}`
