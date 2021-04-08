32d07d41cd94946c7f76ffc40eeaa1b72f939064
Nofu <69540929+Zedruc@users.noreply.github.com>
2021-04-08 13:17:34 +0200

    Improved 8ball command
    Command 1, remaking the whole bot.
    Removed useless functions and checks

--- commands/8ball.js
+++ commands/8ball.js
@@ -10,21 +10,6 @@ module.exports = {
             // Adds the user to the set so that they can't talk for a bit
         }
 
-        // Yoinked from stackoverflow lol
-        //var substrings = ["one", "two", "three"];
-        //var str;
-        //
-        //// Setup
-        //console.log("Substrings: " + substrings.join(","));
-        //
-        //// Try it where we expect a match
-        //str = "this has one";
-        //if (new RegExp(substrings.join("|")).test(str)) {
-        //    console.log("Match using '" + str + "'");
-        //} else {
-        //    console.log("No match using '" + str + "'");
-        //}
-
         const answers = [
             "***Yes***",
             "Lol, **no**",
@@ -32,16 +17,11 @@ module.exports = {
             "Sure why not",
             "Yesn't",
             "Non't",
-            "nerd",
+            "Perhaps",
             "maybe, idk",
-            "8ball is currently on vacation, come back later...",
-            "nope"
+            "Error 420: 8Ball doesn't care",
+            "Nope"
         ];
-
-        const censoredStuffAnswers = [
-            "I've heard Hentai... Hentai is a big ***no no***",
-            "Lolis are illegal, calling the police" //1
-        ]
         const question = message.content.slice(6).split(/ +/);
 
         if (new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?").test(question.join(" ").toString())) {
@@ -49,7 +29,7 @@ module.exports = {
             setTimeout(() => {
                 // Removes the user from the set after a minute
                 talkedRecently.delete(message.author.id);
-            }, 12000); // <- That's not a minute lmao
+            }, 12000);
             return message.channel.send("A link isn't a question");
         }
         talkedRecently.add(message.author.id);
@@ -58,41 +38,10 @@ module.exports = {
             talkedRecently.delete(message.author.id);
         }, 8000);
 
-        let answer_index = Math.floor(Math.random() * (answers.length - 1 - 0)) + 0;
+        // Pick random answer
+        let answer_index = Math.floor(Math.random() * answers.length);
 
-        function sendCensored() {
-            let embed = new Discord.MessageEmbed()
-                .setTitle(":8ball:")
-                .addField("__:question: Question__", question.join(" "))
-                .addField("__:white_check_mark: Answer__", censoredStuffAnswers[answer_index]);
-
-            message.channel.send(embed);
-
-            talkedRecently.add(message.author.id);
-            setTimeout(() => {
-                // Removes the user from the set after a minute
-                talkedRecently.delete(message.author.id);
-            }, 8000); // <- That's not a minute lmao
-            return;
-        }
-
-        for (let i = 0; i < question.length; i++) {
-            if (question[i].toLowerCase().replace(/[^a-zA-Z ]/g, "") == "hentai") {
-                answer_index = 1;
-                sendCensored();
-            }
-            if (question[i].toLowerCase().replace(/[^a-zA-Z ]/g, "") == "loli") {
-                answer_index = 0;
-                sendCensored();
-            }
-            if (question[i].toLowerCase().replace(/[^a-zA-Z ]/g, "") == "lolis") {
-                answer_index = 0;
-                sendCensored();
-
-            }
-        }
-
-        function sendNormal() {
+        function send() {
             let embed = new Discord.MessageEmbed()
                 .setTitle(":8ball:")
                 .addField("__:question: Question__", question.join(" "))
@@ -104,10 +53,10 @@ module.exports = {
             setTimeout(() => {
                 // Removes the user from the set after a minute
                 talkedRecently.delete(message.author.id);
-            }, 8000); // <- That's not a minute lmao
+            }, 8000);
             return;
         }
 
-        sendNormal();
+        send();
     }
 }
