8da77dd82f6f6c058f33741be267a42917a2527c
Nofu <69540929+Zedruc@users.noreply.github.com>
2021-04-29 11:34:25 +0200

    Update main.js

--- main.js
+++ main.js
@@ -128,9 +128,7 @@ client.on('guildMemberAdd', (member) => {
 
 client.on('message', message => {
     if (message.author.id == "647445286695862292" || message.author.id == "646813777492312076" || message.author.id == "602267804288286736" || message.author.id == "733234312220246027") {
-        if (!message.content.startsWith("%sp")) {
-            return;
-        }
+        return;
     }
 
     // =============================================================================
