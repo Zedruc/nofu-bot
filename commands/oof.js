c464bbebe2add68fc9089b573f57d086ca74260f
Nofu <69540929+Zedruc@users.noreply.github.com>
2021-04-29 11:26:10 +0200

    they crashed it

--- commands/oof.js
+++ commands/oof.js
@@ -5,17 +5,22 @@ module.exports = {
     description: "vc test",
     execute(message, args, client) {
         if (!message.member.voice.channel) return message.reply("You have to be in a voice channel!");
-        let vc = message.member.voice.channel;
-        let channel = message.channel;
-        vc.join().then(connection => {
-            message.react("🔊");
-            let dispatcher = connection.play("https://www.myinstants.com/media/sounds/roblox-death-sound_1.mp3") // lul
-            dispatcher.on('finish', end => {
-                vc.leave();
-            });
+        try {
 
-        }).catch(err => {
-            if (err) throw err;
-        })
+            let vc = message.member.voice.channel;
+            let channel = message.channel;
+            vc.join().then(connection => {
+                message.react("🔊");
+                let dispatcher = connection.play("https://www.myinstants.com/media/sounds/roblox-death-sound_1.mp3") // lul
+                dispatcher.on('finish', end => {
+                    vc.leave();
+                });
+
+            }).catch(err => {
+                if (err) throw err;
+            })
+        } catch (error) {
+            throw error
+        }
     }
 }
