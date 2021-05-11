606bc3c9f47d00e994d3bac606e26529cf4e6381
Nofu <69540929+Zedruc@users.noreply.github.com>
2021-05-11 13:00:47 +0200

    E

---
+++ commands/E.js
@@ -0,0 +1,27 @@
+// https://www.myinstants.com/media/sounds/its-in-the-game_TyOFKRF.mp3
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "nut",
+    description: "vc test",
+    execute(message, args, client) {
+        if (!message.member.voice.channel) return message.reply("You have to be in a voice channel!");
+        try {
+
+            let vc = message.member.voice.channel;
+            let channel = message.channel;
+            vc.join().then(connection => {
+                message.react("🔊");
+                let dispatcher = connection.play("https://www.myinstants.com/media/sounds/its-in-the-game_TyOFKRF.mp3") // E
+                dispatcher.on('finish', end => {
+                    vc.leave();
+                });
+
+            }).catch(err => {
+                if (err) throw err;
+            })
+        } catch (error) {
+            throw error
+        }
+    }
+}
